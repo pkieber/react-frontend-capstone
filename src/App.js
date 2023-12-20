2bbb9b6e9281bf389e3f1653cd2b854fc34f6e58
pkieber <113725311+pkieber@users.noreply.github.com>
2023-12-20 14:32:56 +0100

    add html semantic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
